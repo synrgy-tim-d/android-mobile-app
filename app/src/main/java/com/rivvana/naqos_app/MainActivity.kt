722c2bb939115fe59a602525b8ee27396805e193
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2023-01-08 13:42:44 +0700

    Add existing project files to Git

---
+++ app/src/main/java/com/rivvana/naqos_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rivvana.naqos_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
