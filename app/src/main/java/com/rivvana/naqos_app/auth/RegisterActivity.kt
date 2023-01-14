4d86f8eae660e0de03443a96e790ea81ffd7a8e8
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2023-01-14 07:21:50 +0700

    add toolbar

---
+++ app/src/main/java/com/rivvana/naqos_app/auth/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.rivvana.naqos_app.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rivvana.naqos_app.R
+import com.rivvana.naqos_app.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.
+    }
+}
