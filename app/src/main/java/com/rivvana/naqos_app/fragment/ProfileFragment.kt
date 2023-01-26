a030f7d0f8d5636a822e824e51f7378572036069
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2023-01-26 11:55:35 +0700

    add UI profile

--- app/src/main/java/com/rivvana/naqos_app/fragment/ProfileFragment.kt
+++ app/src/main/java/com/rivvana/naqos_app/fragment/ProfileFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.rivvana.naqos_app.R
+import com.rivvana.naqos_app.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
+    private var _binding : FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.layoutToolbar.tvToolbar.text = "Profile"
+
+        binding.layoutToolbar.btnBackToolbar.setOnClickListener{
+            Toast.makeText(context, "Button Back", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
     }
 
     companion object {
